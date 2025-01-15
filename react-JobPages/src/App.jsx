10df54ccaa4545fdc164b5f59cb07a26735ddf9e
Sebastian Luna <newmsloh@gmail.com>
2025-01-15 00:29:13 -0600

    App routing

--- react-JobPages/src/App.jsx
+++ react-JobPages/src/App.jsx
@@ -1,22 +1,16 @@
+import {Route, createBrowserRouter, createRoutesFromElements,RouterProvider} from 'react-router-dom'
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import HomeCards from './components/HomeCards';
 import JobListings from './components/JobListings';
 import ViewAllJobs from './components/ViewAllJobs';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path='/index' element={<h1> My App</h1>} />))
 const App = () => {
-
-
-  return (
-    <>
-    <Navbar />
-    <Hero />
-    <HomeCards />
-    <JobListings />
-    <ViewAllJobs /> 
-    
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
--- react-JobPages/src/components/JobListing.jsx
+++ react-JobPages/src/components/JobListing.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
+import { FaMapMarker } from 'react-icons/fa'
+
 const JobListing = ({job}) => {
     const [showFullDescription,setShowFullDescription] = useState(false) //default state in the ()
     let description = job.description;
@@ -27,7 +29,7 @@ const JobListing = ({job}) => {
     
                   <div className="flex flex-col lg:flex-row justify-between mb-4">
                     <div className="text-orange-700 mb-3">
-                      <i className="fa-solid fa-location-dot text-lg"></i>
+                      <FaMapMarker className='inline text-lg mr-1 mb-1'/>
                       {job.location}
                     </div>
                     <a
