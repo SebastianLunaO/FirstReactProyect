20ea70032c3a97d8a7455a1df791d8aca4303a40
Sebastian Luna <newmsloh@gmail.com>
2025-01-18 21:40:14 -0600

    creating the pages directory and adding the navbar as a layout over every other page

--- react-JobPages/src/App.jsx
+++ react-JobPages/src/App.jsx
@@ -1,14 +1,14 @@
 import {Route, createBrowserRouter, createRoutesFromElements,RouterProvider} from 'react-router-dom'
 import React from 'react'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
-
+import HomePage from './pages/HomePage'
+import MainLayout from './layouts/MainLayout'
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path='/index' element={<h1> My App</h1>} />))
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout />}> 
+  <Route path='/index' element={<HomePage />} />
+  </Route>))
+  
 const App = () => {
   return <RouterProvider router={router} />
 }
