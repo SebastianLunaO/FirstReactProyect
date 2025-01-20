9f2d7bd03cd9fe8ca09b80b5c5d2f5beba3fdbb8
Sebastian Luna <newmsloh@gmail.com>
2025-01-20 16:16:19 -0600

    fetching data

--- react-JobPages/src/components/JobListings.jsx
+++ react-JobPages/src/components/JobListings.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import { useState,useEffect } from 'react';
 import JobListing from './JobListing'
+import Spinner from './Spinner';
 const JobListings = ({isHome = false}) => {
   const [jobs,setJobs] = useState([]);  //const [*name of the function*,name to change the function] = useState(*default value*)
   const [loading,setLoading] = useState(true);
 
   useEffect(()=>{
-    const fetchJobs = async () => {
-      try {
-        const res = await fetch('http://localhost:8000/jobs')
-      const data = await res.json()
-      setJobs(data)
-      } catch (error) {
-        console.log('error fetching ',error)
-      }finally{
-        setLoading(false);
+    
+      const fetchJobs = async () => {
+        const apiURL = isHome ? '/api/jobs?_limit=3' : '/api/jobs'
+        try {
+          const res = await fetch(apiURL)
+        const data = await res.json()
+        setJobs(data)
+        } catch (error) {
+          console.log('error fetching ',error)
+        }finally{
+          setLoading(false);
+        }
+        
       }
-      
-    }
-    fetchJobs();
+    
+      fetchJobs();
   },[]);
   return (
 
@@ -27,11 +31,14 @@ const JobListings = ({isHome = false}) => {
         <h2 className="text-3xl font-bold text-indigo-500 mb-6 text-center">
           {isHome ? 'Browse Jobs':'All Jobs'}
         </h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        
+          {loading ? <Spinner loading={loading}/>: (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {jobs.map((job)=>(
                 <JobListing key={job.id} job={job}/>
-            ))}
-        </div>
+            ))}</div>)}
+            
+        
       </div>
     </section>
     
